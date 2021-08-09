9e299d6040601cf325b5d590fb550b2d3a80848b
Nicolas Muchate <nicolas.muchate@gmail.com>
2021-08-10 01:04:58 +0200

    number of question add

--- game.js
+++ game.js
@@ -46,7 +46,7 @@ fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=mul
 
 //Constants
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 10;
 
 startGame = () => {
   questionCounter = 0;
