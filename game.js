daf596467796b131507d6b79dfda8eb592770d44
Nicolas Muchate <nicolas.muchate@gmail.com>
2021-08-09 15:55:12 +0200

    Create a Head's up display

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -51,8 +53,10 @@ getNewQuestion = () => {
   if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     return window.location.assign('/end.html')
   }
-
   questionCounter++;
+  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`
+
+  // questionCounter++;
   const questionIndex =  Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -83,6 +87,10 @@ choices.forEach(choice => {
     const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
     console.log(classToApply);
 
+    if (classToApply === 'correct') {
+      incrementScore(CORRECT_BONUS);
+    }
+
     selectedChoice.parentElement.classList.add(classToApply);
 
     setTimeout(() => {
@@ -91,5 +99,10 @@ choices.forEach(choice => {
     }, 1000);
   });
 })
+
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+}
 
 startGame();
