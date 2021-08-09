bb85d9836c95e51b95f4b01b45d88d9027b39088
Nicolas Muchate <nicolas.muchate@gmail.com>
2021-08-09 18:38:21 +0200

    Save High Scores in Local Storage

--- end.js
+++ end.js
@@ -3,6 +3,11 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
+console.log(highScores);
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -12,4 +17,21 @@ username.addEventListener('keyup', () => {
 saveHighScore = e => {
   console.log("clicked the save button");
   e.preventDefault();
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+  };
+  highScores.push(score);
+
+  highScores.sort( (a,b) => {
+    return b.score - a.score
+  })
+
+  highScores.splice(5); 
+
+  localStorage.setItem('highScores', JSON.stringify(highScores));
+
+  window.location.assign('/');
+  console.log(highScores);
 };
