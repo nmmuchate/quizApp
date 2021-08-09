29965d267b8be7d233e359b14d7028104e6bb13a
Nicolas Muchate <nicolas.muchate@gmail.com>
2021-08-09 15:30:50 +0200

    Display feedBack for correct/incorrect answers

--- game.js
+++ game.js
@@ -74,8 +74,21 @@ choices.forEach(choice => {
     acceptingAnswers = false;
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset['number'];
-    console.log(selectedAnswer);
-    getNewQuestion();
+
+    // const classToAplly = 'incorrect';
+    //   if(selectedAnswer == currentQuestion.answer) {
+    //     classToAplly = 'correct';   
+    //   }
+
+    const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+    console.log(classToApply);
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
   });
 })
 
